d06b71a446ee97c2cbe87e25680c1b9ab6f7d23f
Biswarup Bhattacharjee <74323889+biswa2210@users.noreply.github.com>
2021-04-05 21:12:42 +0530

    Add files via upload

---
+++ AlgoVisualizer/Functionality4/feedback/src/components/Navbar.js
@@ -0,0 +1,27 @@
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
+export const Navibar = () => {
+    return (
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <Link class="navbar-brand" exact to="/">Algorithm Visualizer</Link>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <Link exact to="/About"><a class="nav-link" href="#">About<span class="sr-only">(current)</span></a></Link>
+      </li>
+      <li class="nav-item">
+        <Link exact to="Userguide"><a class="nav-link" href="#">UserGuide</a></Link>
+      </li>
+      <li class="nav-item">
+        <Link exact to="contactUs"><a class="nav-link" href="#">ContactUs</a></Link>
+      </li>
+    </ul>
+    <Link exact to="/feedback"><button type="button" class="btn btn-outline-light">Submit Feedback</button></Link>
+  </div>
+</nav>
+    );
+}
+export default Navibar;
