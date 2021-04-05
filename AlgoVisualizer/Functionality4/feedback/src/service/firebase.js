18d0168cd49c3433d48403a3657a0417b28059ff
Biswarup Bhattacharjee <74323889+biswa2210@users.noreply.github.com>
2021-04-05 21:16:03 +0530

    Add files via upload

---
+++ AlgoVisualizer/Functionality4/feedback/src/service/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+var firebaseApp = firebase.initializeApp({
+  // Your firebase credentials
+     apiKey: "AIzaSyCHATVE4UgIYyaHghfo4rSafg10lH1h7Vk",
+    authDomain: "algo-visualize.firebaseapp.com",
+    projectId: "algo-visualize",
+    storageBucket: "algo-visualize.appspot.com",
+    messagingSenderId: "1063519860892",
+    appId: "1:1063519860892:web:a09a0a7265a95f1daa1559"
+});
+
+var db = firebaseApp.firestore();
+
+export { db };
