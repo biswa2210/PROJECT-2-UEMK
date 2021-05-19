0ad9643f8e6b7e4a7c5b07af4c6a910180ae63eb
Biswarup Bhattacharjee <74323889+biswa2210@users.noreply.github.com>
2021-05-19 21:13:00 +0530

    Update index.js

--- AlgoVisualizer/Functionality3/disscussionpart/src/index.js
+++ AlgoVisualizer/Functionality3/disscussionpart/src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+                        /*
+CREATED BY BISWARUP BHATTACHARJEE
+EMAIL    : bbiswa471@gmail.com
+PHONE NO : 6290272740
+*/
 import App from './App';
 
 setTimeout(()=>ReactDOM.render(<App />, document.getElementById('root')),3000);
