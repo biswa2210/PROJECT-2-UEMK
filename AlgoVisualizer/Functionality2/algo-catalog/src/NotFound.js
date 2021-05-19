9aae8a49438953f07b5a1a227499c49d4329fc70
Biswarup Bhattacharjee <74323889+biswa2210@users.noreply.github.com>
2021-05-19 20:27:26 +0530

    Update NotFound.js

--- AlgoVisualizer/Functionality2/algo-catalog/src/NotFound.js
+++ AlgoVisualizer/Functionality2/algo-catalog/src/NotFound.js
@@ -1,5 +1,10 @@
 export const NotFound = () => {
     return (
+                /*
+CREATED BY BISWARUP BHATTACHARJEE
+EMAIL    : bbiswa471@gmail.com
+PHONE NO : 6290272740
+*/
         <div id="main">
     	<div class="fof">
         		<h1>Error 404</h1>
