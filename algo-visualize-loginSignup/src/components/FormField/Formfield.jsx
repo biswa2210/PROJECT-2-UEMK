4baa7643bfc0b7738a2babc5dd16b3a7e717a9ec
Biswarup Bhattacharjee <74323889+biswa2210@users.noreply.github.com>
2021-05-19 21:04:20 +0530

    Update Formfield.jsx

--- algo-visualize-loginSignup/src/components/FormField/Formfield.jsx
+++ algo-visualize-loginSignup/src/components/FormField/Formfield.jsx
@@ -1,5 +1,9 @@
 import { ErrorMessage, Field } from 'formik';
-
+/*
+CREATED BY BISWARUP BHATTACHARJEE
+EMAIL    : bbiswa471@gmail.com
+PHONE NO : 6290272740
+*/
 export const FormField = ({ name, label, type = 'text' }) => (
   <label>
     {label}
