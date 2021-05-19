f34243d68cc160a598708715124226559c4a77a8
Biswarup Bhattacharjee <74323889+biswa2210@users.noreply.github.com>
2021-05-19 12:06:29 +0530

    Update server.js

--- AlgoVisualizer/Functionality1/algo-visualize/Searching-Visualizer/server.js
+++ AlgoVisualizer/Functionality1/algo-visualize/Searching-Visualizer/server.js
@@ -6,6 +6,11 @@ app.use("/public", express.static(__dirname + "/public"))
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 })
+/*
+CREATED BY BISWARUP BHATTACHARJEE
+EMAIL    : bbiswa471@gmail.com
+PHONE NO : 6290272740
+*/
 
 app.listen(1337, () => {
   console.log("The server is up and running!");
