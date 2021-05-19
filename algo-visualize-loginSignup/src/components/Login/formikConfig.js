dd42219c7c7ecaa13fd8fdcb0aeccd9a3e3b06c9
Biswarup Bhattacharjee <74323889+biswa2210@users.noreply.github.com>
2021-05-19 21:06:58 +0530

    Update formikConfig.js

--- algo-visualize-loginSignup/src/components/Login/formikConfig.js
+++ algo-visualize-loginSignup/src/components/Login/formikConfig.js
@@ -4,7 +4,11 @@ export const defaultValues = {
   email: '',
   password: '',
 };
-
+/*
+CREATED BY BISWARUP BHATTACHARJEE
+EMAIL    : bbiswa471@gmail.com
+PHONE NO : 6290272740
+*/
 export const validationSchema = Yup.object().shape({
   email: Yup.string().required('Required'),
   password: Yup.string().required('Required'),
