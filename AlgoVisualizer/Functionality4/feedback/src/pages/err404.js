4abea5b9734c04e36a03fed36f214e01f74f57ba
Biswarup Bhattacharjee <74323889+biswa2210@users.noreply.github.com>
2021-05-19 21:10:17 +0530

    Update err404.js

--- AlgoVisualizer/Functionality4/feedback/src/pages/err404.js
+++ AlgoVisualizer/Functionality4/feedback/src/pages/err404.js
@@ -1,3 +1,8 @@
+                        /*
+CREATED BY BISWARUP BHATTACHARJEE
+EMAIL    : bbiswa471@gmail.com
+PHONE NO : 6290272740
+*/
 export const ErrPg = () => {
       return (
         <div id="main">
@@ -5,11 +10,7 @@ export const ErrPg = () => {
                   <h1>Error 404</h1><br></br>
                   <h1>Page Not Found</h1>
     	</div>
-                        /*
-CREATED BY BISWARUP BHATTACHARJEE
-EMAIL    : bbiswa471@gmail.com
-PHONE NO : 6290272740
-*/
+
 </div>
     );
 }
